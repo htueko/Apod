0bd50910e1a4d52ae7876c62589a356e57b7e10f
Htue Ko <htooko1101@gmail.com>
2021-01-31 22:14:51 +0630

    Hookup MainActivity with UI

--- app/src/main/java/com/htueko/apod/ui/MainActivity.kt
+++ app/src/main/java/com/htueko/apod/ui/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.htueko.apod.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.htueko.apod.R
+import com.htueko.apod.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // set the content
+        setContentView(binding.root)
+        // set the action bar
+        setSupportActionBar(binding.appbar)
+        // fix for the bottom navigation view
+        fixBottomNavView(binding)
+        // setup for the navigation
+        setupNavigation()
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = navHostFragment.findNavController()
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigation() {
+        // get the NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+                ?: return
+        // get the NavController from NavHostFragment
+        val navController = navHostFragment.findNavController()
+        // config for toolbar
+        appBarConfiguration = AppBarConfiguration(binding.bottomNavigationView.menu)
+        // setup the toolbar with nav controller alongside toolbar config
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        // setup the bottom navigation
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+    }
+
+    /**
+     * walk around for left margin of the BottomAppBar
+     */
+    private fun fixBottomNavView(binding: ActivityMainBinding) {
+        // remove the shadow from the background of bottom navigation view
+        binding.bottomNavigationView.background = null
+    }
+
 }
