d36c59e135f956883088ad38ddb4ca541e1302ec
Htue Ko <htooko1101@gmail.com>
2021-01-31 21:47:23 +0630

    Add function to read json from assets

---
+++ app/src/main/java/com/htueko/apod/util/Util.kt
@@ -0,0 +1,32 @@
+package com.htueko.apod.util
+
+import android.content.Context
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+
+/**
+ * convert file to StringBuilder
+ */
+fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+    var inputStream: InputStream? = null
+    val builder = StringBuilder()
+    try {
+        //jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        var jsonString: String? = null
+        inputStream = context.resources.assets.open(fileName)
+        val bufferedReader = BufferedReader(
+            InputStreamReader(inputStream, "UTF-8")
+        )
+        while (bufferedReader.readLine().also { jsonString = it } != null) {
+            builder.append(jsonString)
+        }
+    } catch (ioException: IOException) {
+        ioException.printStackTrace()
+        return null
+    } finally {
+        inputStream?.close()
+    }
+    return String(builder)
+}
