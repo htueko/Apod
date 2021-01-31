f300bb54a3b994a81522e2e221ec1ed7ed2e6e8e
Htue Ko <htooko1101@gmail.com>
2021-01-31 22:28:21 +0630

    Hookup LocalFragment with UI

--- app/src/main/java/com/htueko/apod/ui/fragment/LocalFragment.kt
+++ app/src/main/java/com/htueko/apod/ui/fragment/LocalFragment.kt
@@ -1,3 +1,76 @@
 package com.htueko.apod.ui.fragment
 
-class LocalFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.htueko.apod.databinding.FragmentLocalBinding
+import com.htueko.apod.ui.adapter.ApodAdapter
+import com.htueko.apod.ui.viewmodel.LocalViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+class LocalFragment : BaseFragment<FragmentLocalBinding, LocalViewModel>() {
+
+    override val viewModel: LocalViewModel by viewModels()
+    private val mAdapter = ApodAdapter() {
+        // todo send Apod object for detail view
+    }
+
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentLocalBinding = FragmentLocalBinding.inflate(layoutInflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        updateData()
+        observeData()
+    }
+
+    /**
+     * when the internet connection is available
+     */
+    override fun onConnectionAvailable() {
+        hideNetworkDialog()
+    }
+
+    /**
+     * when the internet connection is not available
+     */
+    override fun onConnectionUnavailable() {
+        showNetworkDialog()
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            spanCount = 2
+            adapter = mAdapter
+        }
+    }
+
+    private fun updateData() {
+        // get the local data set
+        val data = getLocalData()
+        // update the livedata at ViewModel
+        viewModel.updateData(data)
+    }
+
+    /**
+     * observe the change of the livedata from ViewModel and update the Adapter data set
+     */
+    private fun observeData() {
+        viewModel.apodData.observe(viewLifecycleOwner, Observer {
+            mAdapter.updateData(it)
+        })
+    }
+
+}
